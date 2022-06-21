b5fd1f3c65d5582d5667b75cb51900212aa92441
Milo Roberts <mroberts18@bristolcc.edu>
2022-06-21 12:58:55 -0400

    Lesson 10
    Lessons 1-10 completed

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+
+        <h1><a href="/posts/my-first-post">My First Post</a>
+            </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis at quasi facilis modi. Totam rem ut sapiente quos, soluta, provident facilis similique aperiam eius blanditiis laboriosam quam officia ipsum tempora consectetur perspiciatis vero ullam doloremque a accusantium repudiandae dolor illum recusandae. Vero, ad iusto debitis ex tenetur repellat, blanditiis est quisquam fugit numquam magni laborum accusamus aliquid? Asperiores a aspernatur incidunt quaerat quam, exercitationem eligendi dolorem, minus, quas mollitia nostrum impedit? Voluptatibus qui illum iste nam numquam labore officia, eos ex ducimus quaerat inventore veniam aut dignissimos tempore dolorum sunt doloremque itaque architecto corrupti. Quod quos vero ut voluptate eos.</p>
+    </article>
+    <article>
+
+        <h1><a href="/posts/my-second-post">My Second Post</a>
+            </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis at quasi facilis modi. Totam rem ut sapiente quos, soluta, provident facilis similique aperiam eius blanditiis laboriosam quam officia ipsum tempora consectetur perspiciatis vero ullam doloremque a accusantium repudiandae dolor illum recusandae. Vero, ad iusto debitis ex tenetur repellat, blanditiis est quisquam fugit numquam magni laborum accusamus aliquid? Asperiores a aspernatur incidunt quaerat quam, exercitationem eligendi dolorem, minus, quas mollitia nostrum impedit? Voluptatibus qui illum iste nam numquam labore officia, eos ex ducimus quaerat inventore veniam aut dignissimos tempore dolorum sunt doloremque itaque architecto corrupti. Quod quos vero ut voluptate eos.</p>
+    </article>
+    <article>
+
+        <h1><a href="/posts/my-third-post">My Third Post</a>
+            </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis at quasi facilis modi. Totam rem ut sapiente quos, soluta, provident facilis similique aperiam eius blanditiis laboriosam quam officia ipsum tempora consectetur perspiciatis vero ullam doloremque a accusantium repudiandae dolor illum recusandae. Vero, ad iusto debitis ex tenetur repellat, blanditiis est quisquam fugit numquam magni laborum accusamus aliquid? Asperiores a aspernatur incidunt quaerat quam, exercitationem eligendi dolorem, minus, quas mollitia nostrum impedit? Voluptatibus qui illum iste nam numquam labore officia, eos ex ducimus quaerat inventore veniam aut dignissimos tempore dolorum sunt doloremque itaque architecto corrupti. Quod quos vero ut voluptate eos.</p>
+    </article>
+</body>
