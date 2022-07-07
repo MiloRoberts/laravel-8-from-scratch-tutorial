ea8f2f1be4ac5f1baaed9993fcdaa50b93e7d585
Milo Roberts <mroberts18@bristolcc.edu>
2022-07-06 20:24:58 -0400

    Lesson 44
    Lesson 43-44 completed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,10 +2,10 @@
 <x-layout>
     @include ('posts._header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <!-- test condition below by emptying database of posts -->
         @if ($posts->count())
-        <x-posts-grid :posts="$posts"/>
-        <!-- don't forget this stuff below as well -->
+            <x-posts-grid :posts="$posts"/>
+            
+            {{ $posts->links() }}
         @else
             <p class="text-center">No posts yet. Please check back later.</p>
         @endif
